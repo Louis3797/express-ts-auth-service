f9fd0df6a7241fa51ef5faf3617ece059ac5433c
Louis <louis-kaan2002@web.de>
2023-03-08 15:54:50 +0100

    updated .env and env.d.ts

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,7 +1,12 @@
 declare namespace NodeJS {
   interface ProcessEnv {
-    readonly NODE_ENV: string;
+    readonly NODE_ENV: 'production' | 'development' | 'staging';
     readonly PORT: number;
     readonly CORS_ORIGIN: string;
+    readonly JWT_SECRET: string;
+    readonly JWT_EXPIRE: string;
+    readonly MYSQL_DATABASE: string;
+    readonly MYSQL_ROOT_PASSWORD: string;
+    readonly DATABASE_URL: string;
   }
 }
