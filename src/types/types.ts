7cbc2dca897f1526015d7487632b403cb7eb8f48
Louis <louis-kaan2002@web.de>
2023-03-09 00:58:01 +0100

    moved .d.ts files in types folder and added stricter req res types

---
+++ src/types/types.ts
@@ -0,0 +1,44 @@
+import type { NextFunction, Request, Response } from 'express';
+import type { Query, Send } from 'express-serve-static-core';
+
+// See this for the following types
+// https://stackoverflow.com/questions/34508081/how-to-add-typescript-definitions-to-express-req-res
+// https://stackoverflow.com/questions/61132262/typescript-deep-partial
+
+type DeepPartial<T> = T extends object
+  ? {
+      [P in keyof T]?: DeepPartial<T[P]>;
+    }
+  : T;
+
+// More strictly typed Express.Request type
+export type TypedRequest<
+  ReqBody = Record<string, unknown>,
+  QueryString = Record<string, unknown>
+> = Request<
+  Record<string, unknown>,
+  Record<string, unknown>,
+  DeepPartial<ReqBody>,
+  DeepPartial<QueryString>
+>;
+
+// More strictly typed express middleware type
+export type ExpressMiddleware<
+  ReqBody = Record<string, unknown>,
+  Res = Record<string, unknown>,
+  QueryString = Record<string, unknown>
+> = (
+  req: TypedRequest<ReqBody, QueryString>,
+  res: Response<Res>,
+  next: NextFunction
+) => Promise<void> | void;
+
+// Example usage from Stackoverflow:
+// type Req = { email: string; password: string };
+
+// type Res = { message: string };
+
+// export const signupUser: ExpressMiddleware<Req, Res> = async (req, res) => {
+//   /* strongly typed `req.body`. yay autocomplete 🎉 */
+//   res.json({ message: 'you have signed up' }) // strongly typed response obj
+// };
