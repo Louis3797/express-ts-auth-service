9da6475ad36e8419490c7b81ad38e2aedea3368a
Louis <louis-kaan2002@web.de>
2023-03-16 16:31:51 +0100

    added new midlewares and routes

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,25 @@
-import express, { Express, Request, Response } from 'express';
+import express, { Express } from 'express';
 import cors from 'cors';
 import helmet from 'helmet';
 import compression from 'compression';
 import compressFilter from './utils/compressFilter.util';
 import config from './config/config';
+import { authRouter, userRouter } from './routes/v1';
+import isAuth from './middleware/isAuth';
 
 const app: Express = express();
+
+// Helmet is used to secure this app by configuring the http-header
+app.use(helmet());
+
+// parse json request body
+app.use(express.json());
+
+// parse urlencoded request body
+app.use(express.urlencoded({ extended: true }));
+
+// Compression is used to reduce the size of the response body
+app.use(compression({ filter: compressFilter }));
 
 app.use(
   cors({
@@ -15,14 +29,18 @@ app.use(
   })
 );
 
-// Helmet is used to secure this app by configuring the http-header
-app.use(helmet());
+// Todo -> rate limiting auth routes ??
 
-// Compression is used to reduce the size of the response body
-app.use(compression({ filter: compressFilter }));
+app.use('/api/v1/auth', authRouter);
 
-app.get('/', (_req: Request, res: Response) => {
-  res.send('Hello World!');
+app.use('/api/v1/user', userRouter);
+
+app.use('/secret', isAuth, (req, res) => {
+  res.json({
+    message: 'You can see me',
+  });
 });
+
+// Todo -> Error handling
 
 export default app;
