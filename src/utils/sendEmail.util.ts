0e922e1d3075c6e2f80c821b74685480cf7115de
Louis <louis-kaan2002@web.de>
2023-03-18 19:03:09 +0100

    changes for debugging

--- src/utils/sendEmail.util.ts
+++ src/utils/sendEmail.util.ts
@@ -2,13 +2,14 @@ import logger from '../middleware/logger';
 import transporter from '../config/nodemailer';
 
 export const sendResetEmail = (email: string, token: string) => {
-  const resetLink = `https://example.com/reset-password/${token}`;
+  const resetLink = `http://localhost:4040/api/v1/reset-password/${token}`;
   const mailOptions = {
     from: 'your_email@example.com',
     to: email,
     subject: 'Password reset',
     html: `Please click <a href="${resetLink}">here</a> to reset your password.`,
   };
+  console.log(resetLink);
   transporter.sendMail(mailOptions, (error, info) => {
     if (error) {
       logger.error(error);
@@ -19,13 +20,14 @@ export const sendResetEmail = (email: string, token: string) => {
 };
 
 export const sendVerifyEmail = (email: string, token: string) => {
-  const verifyLink = `https://example.com/verify-email/${token}`;
+  const verifyLink = `http://localhost:4040/api/v1/verify-email/${token}`;
   const mailOptions = {
     from: 'your_email@example.com',
     to: email,
     subject: 'Email verification',
     html: `Please click <a href="${verifyLink}">here</a> to verify your email.`,
   };
+  console.log(verifyLink);
   transporter.sendMail(mailOptions, (error, info) => {
     if (error) {
       logger.error(error);
