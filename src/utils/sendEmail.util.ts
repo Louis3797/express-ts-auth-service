55184a7703c5e3241e741ffcb263b8204d2a17be
Louis <louis-kaan2002@web.de>
2023-03-18 21:59:33 +0100

    added errorHandler middleware, and rate limiter for auth

--- src/utils/sendEmail.util.ts
+++ src/utils/sendEmail.util.ts
@@ -1,15 +1,15 @@
 import logger from '../middleware/logger';
 import transporter from '../config/nodemailer';
+import config from '../config/config';
 
 export const sendResetEmail = (email: string, token: string) => {
-  const resetLink = `http://localhost:4040/api/v1/reset-password/${token}`;
+  const resetLink = `${config.server_url}/api/v1/reset-password/${token}`;
   const mailOptions = {
-    from: 'your_email@example.com',
+    from: config.email_from,
     to: email,
     subject: 'Password reset',
     html: `Please click <a href="${resetLink}">here</a> to reset your password.`,
   };
-  console.log(resetLink);
   transporter.sendMail(mailOptions, (error, info) => {
     if (error) {
       logger.error(error);
@@ -20,14 +20,13 @@ export const sendResetEmail = (email: string, token: string) => {
 };
 
 export const sendVerifyEmail = (email: string, token: string) => {
-  const verifyLink = `http://localhost:4040/api/v1/verify-email/${token}`;
+  const verifyLink = `${config.server_url}/api/v1/verify-email/${token}`;
   const mailOptions = {
-    from: 'your_email@example.com',
+    from: config.email_from,
     to: email,
     subject: 'Email verification',
     html: `Please click <a href="${verifyLink}">here</a> to verify your email.`,
   };
-  console.log(verifyLink);
   transporter.sendMail(mailOptions, (error, info) => {
     if (error) {
       logger.error(error);
